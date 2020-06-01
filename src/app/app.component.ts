f93c9703ecabd35291860c017ee1556a67e899dd
Taranti <38500851+Taranti@users.noreply.github.com>
2020-06-01 12:46:39 +0200

    Test version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
+import { MatInputModule } from '@angular/material/input';
+import { InfosComponent } from './infos/infos.component';
 import { Component } from '@angular/core';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
+import * as _ from 'lodash';
+import { Tile } from './models/tile.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lottery';
+  dialogRef: MatDialogRef<InfosComponent>
+  baseUrl ="https://pure-retreat-07717.herokuapp.com/"
+  lottery:Tile[]=[]
+  constructor(public dialog:MatDialog,private http: HttpClient,private _snackBar: MatSnackBar) {
+    this.http.get(this.baseUrl+'lottery').subscribe(res=>{
+      this.lottery=_.map(res,(dbTile)=>{
+        const tile=new Tile()
+        tile.id=dbTile.id 
+        tile.empty=dbTile.empty
+        return tile
+      })
+      console.log(this.lottery)
+    })
+   
+  }
+
+  openDialog(tile:Tile) {
+     this.dialog.open(InfosComponent,{data:{tile}});
+  }
+  async fill(tile:Tile){
+    //if(tile.empty){
+      console.log(tile.id)
+      //tile.empty=false
+      let dbTile;
+      await this.http.get(this.baseUrl+'lottery/'+tile.id).subscribe((resp:any)=>{
+        console.log(resp)
+        if(resp.empty){
+          this.dialogRef=this.dialog.open(InfosComponent,{data:tile})
+          this.dialogRef.afterClosed().subscribe(tileTest=>{
+            console.log(tileTest)
+            if(!tileTest){
+              tile.empty=true
+              this.http.put(this.baseUrl+'lottery',tile).subscribe(resp=>console.log(resp))
+            }
+          })
+        }
+        else{
+          this._snackBar.open('Cette case est déjà remplie', 'OK', {
+            duration: 5000,
+            panelClass: ['danger']
+          });
+        }
+      })
+      
+      
+    /*}
+    else{
+      console.log(tile)
+    }*/
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { InfosComponent } from './infos/infos.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatDialogModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    HttpClientModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[InfosComponent]
 })
 export class AppModule { }
